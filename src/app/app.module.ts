1236759ad5e823018d6b97416950743efb5f1731
jireh padua <moof44@yahoo.com>
2021-02-20 19:43:13 +0800

    save changes to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './navigation/navigation.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { HeaderComponent } from './header/header.component';
+import { MaterialModule } from './modules/material.module';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    HeaderComponent,
+    SidenavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/material.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+
+@NgModule({
+    imports: [
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
+        MatIconModule
+    ],
+    exports:[
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
+        MatIconModule
+    ]
+})
+export class MaterialModule{
+
+}
